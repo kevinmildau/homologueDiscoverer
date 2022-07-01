84ae82c0f72632625a44a127b5cfb4b4856eae3c
Kevin Mildau <kmildau@rstudio.anc.univie.ac.at>
2022-07-01 11:44:08 +0200

    extended vignette and fixed data loading issue

--- R/data.R
+++ R/data.R
@@ -1,7 +1,6 @@
-#' MTBLS1358 dataset
+#' mtbls1358
 #'
 #' This dataset is used as an example peak table in the vignette of homologueDiscoverer. Peak table with peak_id, mz, rt and intensity columns from the MTBLS1358 dataset.
-#'
 #' @format A data frame with 16,106 rows and 4 variables:
 #' \describe{
 #'   \item{peak_id}{unique identifier for each feature, type integer}
@@ -9,5 +8,4 @@
 #'   \item{rt}{retention time in seconds, type double}
 #'   \item{intensity}{average feature intensity across the different samples of the original MTBLS1358 dataset, type double}
 #' }
-#' @source
 "mtbls1358"
